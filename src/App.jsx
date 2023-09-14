25c92f4b79ef84dc21318f65758eb715ec3c751b
saiful islam <saifulislam975317@gmail.com>
2023-09-14 22:55:09 +0600

    added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import router from "./Routes/Routes";
 
 function App() {
-  return (
-    <>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
-    </>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
