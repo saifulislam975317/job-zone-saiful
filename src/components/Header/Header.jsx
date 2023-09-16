6495838437168e5a1261e8ef2d539b0ad849dd3a
saiful islam <saifulislam975317@gmail.com>
2023-09-16 13:40:52 +0600

    update info

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,27 @@
 import image from "../../assets/images/image.jpg";
 const Header = () => {
   return (
-    <div className="hero bg-gray-100">
-      <div className="hero-content flex-col p-24 lg:flex-row-reverse">
-        <img src={image} className="max-w-sm rounded-lg shadow-2xl" />
-        <div className="mr-24">
-          <h1 className="text-4xl font-sans font-bold">
-            Building Your <br /> Bright Career Mission <br />
-            <span className="text-purple-400">with Expertise</span>
-          </h1>
-          <p className="py-6">
-            our vision is to empower individuals and businesses <br />
-            through innovative software solutions. We strive to be the catalyst
-            <br />
-            for digital transformation, enabling success, growth,
-            <br />
-            and excellence in the ever-evolving technological landscape.
-          </p>
-          <button className="btn text-white bg-gradient-to-r  from-primary to-info">
-            Get Started
-          </button>
-        </div>
+    <div className="md:flex lg:flex-row-reverse justify-center items-center gap-x-12  md:mt-8">
+      <div>
+        <img
+          className="w-1/3  md:w-full rounded-lg shadow-2xl"
+          src={image}
+          alt=""
+        />
+      </div>
+      <div className="p-4  space-y-2">
+        <h1 className="text-3xl font-sans font-bold">
+          Building Your <br /> Bright Career Mission <br />
+          <span className="text-purple-400">with Expertise</span>
+        </h1>
+        <p className="text-sm">
+          our vision is to empower individuals and
+          <br /> businesses through innovative software solutions.
+          <br /> We strive to be the catalyst for digital transformation.
+        </p>
+        <button className="btn text-white bg-gradient-to-r  from-primary to-info">
+          Get Started
+        </button>
       </div>
     </div>
   );
--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -5,8 +5,45 @@ const JobDetails = () => {
 
   return (
     <div>
-      <h1>This is job details</h1>
-      <h1>details {details.id}</h1>
+      <h1 className="md:text-center font-bold text-3xl mt-8 p-2">
+        Job details
+      </h1>
+      <div className="grid md:grid-cols-[800px,300px] my-8 gap-4 ">
+        <div className="px-4 overflow-auto">
+          <p>
+            <span className="text-lg font-bold ">Job Description:</span>
+            {details.description}
+          </p>
+          <p>
+            <span className="text-lg font-bold">Job Responsibility: </span>
+            {details.responsibility}
+          </p>
+          <p>
+            <span className="text-lg font-bold">Job Requirements: </span>
+            {details.requirements}
+          </p>
+          <p>
+            <span className="text-lg font-bold">Experiences: </span>
+            {details.experiences}
+          </p>
+        </div>
+        <div>
+          <div className="bg-slate-300 opacity-100   p-8 rounded-lg md:h-1/2">
+            <h1 className="border-b-2 border-black mt-4">Job details</h1>
+            <div>
+              <h1>Job Title: {details.jobTitle}</h1>
+              <h2>Salary: {details.salary} (Per Month)</h2>
+            </div>
+            <div>
+              <h1 className="border-b-2 border-black">Contact Information</h1>
+              <p>Phone: {details.phone}</p>
+              <p>Email: {details.email}</p>
+              <p>Address: {details.address}</p>
+            </div>
+          </div>
+          <button className="btn btn-info w-full mt-2">Apply Now</button>
+        </div>
+      </div>
     </div>
   );
 };
